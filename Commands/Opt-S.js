e65e3d5bfb7438f771cf054ae66a9c1f91821a32
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2021-12-09 14:49:08 +0530

    Update Opt-S.js

--- Commands/Opt-S.js
+++ Commands/Opt-S.js
@@ -3,8 +3,9 @@ function displayFileS(content){
   let duplicateContent = [];
 
   for(let j=0;j<content.length;j++){
-    if(content[j]=='\r') continue;
-      duplicateContent.push(content[j], '\r');
+    if(j==0 || content[j]!=content[j-1]){
+      duplicateContent.push(content[j]);
+    }
   }
 
   //Remove the last carriage return.
