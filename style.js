519930396e9dfb760e2798e836b60f560930550d
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2021-12-09 14:20:39 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,17 @@
+/* Styles */
+/* Imports */
+
+const cls = require('cli-color');
+
+/* Line Numbering */
+
+const lns = cls.xterm(15);
+
+/* Content */
+
+const cns = cls.xterm(19);
+
+/* Exports */
+
+exports.lns = lns;
+exports.cns = cns;
